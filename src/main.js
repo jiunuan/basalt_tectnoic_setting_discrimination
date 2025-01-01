1d628fe32b6744cefca57a9b708a16337d20f01f
jiunuan <2361005996@qq.com>
2025-01-01 20:35:12 +0800

    [deploy]

--- src/main.js
+++ src/main.js
@@ -7,10 +7,11 @@ import i18n from './i18n'
 import * as tf from '@tensorflow/tfjs'
 
 // 初始化 TensorFlow.js
-await tf.ready()
-console.log('TensorFlow.js 初始化成功，后端:', tf.getBackend())
-
-const app = createApp(App)
-app.use(ElementPlus)
-app.use(i18n)
-app.mount('#app')
+tf.ready().then(() => {
+  console.log('TensorFlow.js 初始化成功，后端:', tf.getBackend())
+  
+  const app = createApp(App)
+  app.use(ElementPlus)
+  app.use(i18n)
+  app.mount('#app')
+})
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tectnoic_setting_discrimination_front/',
   plugins: [vue()],
   publicDir: 'public',
   server: {
